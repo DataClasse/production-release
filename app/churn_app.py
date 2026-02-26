d0678b3bb9a531f870a44a767b628270b57dfe2b
Dmitry Shcherbackov <AIOpenData@gmail.com>
2026-02-26 16:52:46 +0300

    docs: remove mle-sprint3 from churn_app docstring

--- app/churn_app.py
+++ app/churn_app.py
@@ -3,14 +3,9 @@
 from fastapi import FastAPI
 from fast_api_handler import FastApiHandler
 
-"""
-Пример запуска из директории mle-sprint3/app:
-uvicorn churn_app:app --reload --port 8081 --host 0.0.0.0
+# Запуск: из корня репо: uvicorn app.churn_app:app --reload --port 8081 --host 0.0.0.0
+# Документация: http://127.0.0.1:8081/docs
 
-Для просмотра документации API и совершения тестовых запросов зайти на http://127.0.0.1:8081/docs
-
-Если используется другой порт, то заменить 8081 на этот порт
-"""
 
 # создаём FastAPI-приложение 
 app = FastAPI()
